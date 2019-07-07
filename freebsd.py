9c8765eee20847ff2c1d25cb7ffc751e29d61313
Roland Smith <rsmith@xs4all.nl>
2019-07-08 00:48:14 +0200

    Added hostuuid and osrevision.

--- freebsd.py
+++ freebsd.py
@@ -4,7 +4,7 @@
 # Copyright © 2019 R.F. Smith <rsmith@xs4all.nl>.
 # SPDX-License-Identifier: MIT
 # Created: 2019-07-07T23:56:25+0200
-# Last modified: 2019-07-08T00:26:05+0200
+# Last modified: 2019-07-08T00:47:11+0200
 """Python bindings for some FreeBSD library calls"""
 
 import ctypes
@@ -24,9 +24,14 @@ def to_degC(value):
     return round(int.from_bytes(value, byteorder='little') / 10 - 273.15, 1)
 
 
+def to_string(value):
+    """Convert binary sysctl value to UTF-8 string."""
+    return value.strip(b'\x00').decode('utf-8')
+
+
 def sysctlbyname(name, buflen=4, convert=None):
     """
-    Python wrapper for sysctlbyname(3) on FreeBSD.
+    Python wrapper for sysctlbyname(3).
 
     Arguments:
         name (str): Name of the sysctl to query
@@ -51,7 +56,7 @@ def sysctlbyname(name, buflen=4, convert=None):
 
 def sysctl(name, buflen=4, convert=None):
     """
-    Python wrapper for sysctl(3) on FreeBSD.
+    Python wrapper for sysctl(3).
 
     Arguments:
         name: list or tuple of integers.
@@ -81,7 +86,7 @@ def sysctl(name, buflen=4, convert=None):
 
 def setproctitle(name):
     """
-    Change the name of the process on FreeBSD.
+    Change the name of the process.
 
     Arguments:
         name (bytes/str): the new name for the process.
@@ -91,3 +96,15 @@ def setproctitle(name):
     fmt = ctypes.c_char_p(b'-%s')
     value = ctypes.c_char_p(name)
     _libc.setproctitle(fmt, value)
+
+
+def hostuuid():
+    """Returns the UUID of this host."""
+    rv = sysctlbyname('kern.hostuuid', buflen=40, convert=to_string)
+    return rv
+
+
+def osrevision():
+    """Returns operating system revision"""
+    rv = sysctlbyname('kern.osrevision', convert=to_int)
+    return rv
